891c0e08f13859ad927d0ea05ad76d2a274bcd7e
Dean Gurvitz <deansg@gmail.com>
2024-05-21 10:41:18 +0300

    Copy fields map if it exists in order to not affect parent context
    Closes #5

--- context.go
+++ context.go
@@ -16,10 +16,20 @@ func WithValue(parent context.Context, key string, val any) context.Context {
 		panic("cannot create context from nil parent")
 	}
 	if v, ok := parent.Value(fields).(*sync.Map); ok {
-		v.Store(key, val)
-		return context.WithValue(parent, fields, v)
+		mapCopy := copySyncMap(v)
+		mapCopy.Store(key, val)
+		return context.WithValue(parent, fields, mapCopy)
 	}
 	v := &sync.Map{}
 	v.Store(key, val)
 	return context.WithValue(parent, fields, v)
+}
+
+func copySyncMap(m *sync.Map) *sync.Map {
+	var cp sync.Map
+	m.Range(func(k, v interface{}) bool {
+		cp.Store(k, v)
+		return true
+	})
+	return &cp
 }
